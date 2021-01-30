1eef02ca640e5c2a1c33fd07ab779019f0322bf9
Sandro Papais <papais.sandro@gmail.com>
2021-01-30 22:44:15 +0000

    added BRISK results files

--- src/describe_keypoints.cpp
+++ src/describe_keypoints.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <numeric>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -11,6 +12,16 @@ using namespace std;
 
 void descKeypoints1()
 {
+    // Intialize output file
+    ofstream outfile;
+    outfile.open("../out/results.txt");
+    if (!outfile.is_open())
+    {
+        cout << "Unable to open file" << endl;
+        return;
+    }
+
+
     // load image from file and convert to grayscale
     cv::Mat imgGray;
     cv::Mat img = cv::imread("../images/img1.png");
@@ -24,6 +35,7 @@ void descKeypoints1()
     detector->detect(imgGray, kptsBRISK);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << "BRISK detector with n= " << kptsBRISK.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+    outfile << "BRISK detector with n= " << kptsBRISK.size() << " keypoints in " << 1000 * t / 1.0 << " ms\n";
 
     cv::Ptr<cv::DescriptorExtractor> descriptor = cv::BRISK::create();
     cv::Mat descBRISK;
@@ -31,6 +43,7 @@ void descKeypoints1()
     descriptor->compute(imgGray, kptsBRISK, descBRISK);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << "BRISK descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+    outfile << "BRISK descriptor in " << 1000 * t / 1.0 << " ms\n";
 
     // visualize results
     cv::Mat visImage = img.clone();
@@ -38,12 +51,17 @@ void descKeypoints1()
     string windowName = "BRISK Results";
     cv::namedWindow(windowName, 1);
     imshow(windowName, visImage);
-    cv::waitKey(0);
+    imwrite("../out/BRISK.jpg", visImage);
 
     // TODO: Add the SIFT detector / descriptor, compute the 
     // time for both steps and compare both BRISK and SIFT
     // with regard to processing speed and the number and 
     // visual appearance of keypoints.
+
+
+    // Clean up workspace
+    outfile.close();
+    cv::waitKey(0);
 
 }
 
